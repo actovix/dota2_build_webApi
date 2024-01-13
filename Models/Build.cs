11e94e6e64aa4f58da7a131345265afca3d157a8
actoviq <actovix@proton.me>
2024-01-13 21:13:02 +0400

    Base added

---
+++ Models/Build.cs
@@ -0,0 +1,9 @@
+
+
+namespace dota2_heroes_webApi.Models;
+
+public class Build
+{
+    public string LinkToPage { get; set; }
+    public List<Item> Items { get; set; }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,10 @@
+
+namespace dota2_heroes_webApi.Models;
+
+public class Item
+{
+    public string Name { get; set; }
+    public string PictureLink { get; set; }
+    public string TimeTiPurchase { get; set; }
+
+}
